fcefa9e84aa671d6dee1154aa0d7e8b9c348d0f9
Mukesh Patil <mp743032@gmail.com>
2022-08-04 15:02:36 +0530

    EMS 02 Get all Data method insert

--- src/main/java/com/Biikad/controller/EmployeeController.java
+++ src/main/java/com/Biikad/controller/EmployeeController.java
@@ -1,8 +1,11 @@
 package com.Biikad.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +25,16 @@ public class EmployeeController {
 		Employee employee2 = serviceIMPL.savedata(employee);
 		
 		return new ResponseEntity<Employee>(employee2, HttpStatus.OK) ;
+		
+		
+	}
+	@GetMapping(value="/getData" , produces="/application/json")
+	public List <Employee> getalldata(){
+		
+		List<Employee> list = serviceIMPL.getalldata();
+		
+		
+		return list;
 		
 		
 	}
