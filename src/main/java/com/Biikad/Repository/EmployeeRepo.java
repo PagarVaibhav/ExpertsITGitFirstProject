ca9cf870c23e7abace8430fdbe6f6b2cf1c58faa
Viki488 <cahudhari.viki66@gmail.com>
2022-08-04 13:57:37 +0530

    EMS 04 Method Added

--- src/main/java/com/Biikad/Repository/EmployeeRepo.java
+++ src/main/java/com/Biikad/Repository/EmployeeRepo.java
@@ -3,9 +3,14 @@ package com.Biikad.Repository;
 import java.io.Serializable;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.Biikad.Model.Employee;
 
+
+@Repository
 public interface EmployeeRepo extends JpaRepository<Employee, Serializable> {
+	
+	public Employee findByDept (String dept);
 
 }
