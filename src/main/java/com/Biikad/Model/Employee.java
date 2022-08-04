96accf7ed5ca7628e37cf4a5954e119f8bcc8ecf
PagarVaibhav <vaibhavpagar99@gmail.com>
2022-08-04 12:25:27 +0530

    EMS 01

---
+++ src/main/java/com/Biikad/Model/Employee.java
@@ -0,0 +1,71 @@
+package com.Biikad.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer eid;
+	
+	private String ename;
+	
+	private String edept;
+	
+	private String edesignation;
+	
+	private double esalary;
+
+	
+	public Integer getEid() {
+		return eid;
+	}
+
+	public void setEid(Integer eid) {
+		this.eid = eid;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public String getEdept() {
+		return edept;
+	}
+
+	public void setEdept(String edept) {
+		this.edept = edept;
+	}
+
+	public String getEdesignation() {
+		return edesignation;
+	}
+
+	public void setEdesignation(String edesignation) {
+		this.edesignation = edesignation;
+	}
+
+	public double getEsalary() {
+		return esalary;
+	}
+
+	public void setEsalary(double esalary) {
+		this.esalary = esalary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", ename=" + ename + ", edept=" + edept + ", edesignation=" + edesignation
+				+ ", esalary=" + esalary + "]";
+	}
+	
+	
+}
