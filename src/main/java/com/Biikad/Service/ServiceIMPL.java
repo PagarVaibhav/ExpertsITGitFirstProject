68d850ae8dabf93ce1779d4933e070b0d41e4639
kapilpatil95 <kapilpatil9621@gmail.com>
2022-08-04 14:44:38 +0530

    EM 03 DATA SAVED

--- src/main/java/com/Biikad/Service/ServiceIMPL.java
+++ src/main/java/com/Biikad/Service/ServiceIMPL.java
@@ -1,5 +1,7 @@
 package com.Biikad.Service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +23,14 @@ public class ServiceIMPL {
 		return employee2;
 		
 		}
+	
+	public List <Employee> getalldata() {
+		
+		List<Employee> list = employeeRepo.findAll();
+		
+		return list;
+		
+	}
 	
 	
 }
